b4ee092a25ecae99118d8f1e3ed611217295fa43
Arturnix <artur.zgodka.programista@gmail.com>
2024-09-24 13:47:41 +0200

    Added Main.java

---
+++ src/main/java/pl/ArturZgodka/Main.java
@@ -0,0 +1,13 @@
+package pl.ArturZgodka;
+
+import pl.ArturZgodka.LeetCode.RemoveDuplicatesII;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //System.out.println(isUniqeChars("abcc"));
+        //RemoveElementsFromArray.removeElement(new int[]{3,2,2,3}, 3);
+        //RemoveDuplicates.removeDuplicates(new int[]{0,0,1,1,2});
+        //RemoveDuplicatesII.removeDuplicates(new int[]{1});
+    }
+}
